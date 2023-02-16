7231e819a41f05572dd38dabeb5594445341e285
Sanhil143 <sanhilrai143@gmail.com>
2023-02-16 23:03:47 +0530

    add

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -4,14 +4,50 @@ const { isValidString, isValidNum } = require('../validations/validation')
 Items = db.items
 
 const createItem = async (req, res) => {
+      try {
       let data = req.body
+      const {productName, stockPrice, sellPrice} = data
+      if(productName){
+            let proName = await Items.findOne({ where: { productName: productName } })
+            if (proName) {
+                  return res.status(400).send({ status: false, message: "Name is already exist" })
+            }
+            if (!isValidString(productName)) {
+                  return res.status(400).send({ status: false, message: "Please provide valid product name" })
+            }
+      }
+      if (sellPrice) {
+            if (!sellPrice) {
+                  return res.status(400).send({ status: false, message: "Please provide valid sellPrice" })
+            }
+            if (!isValidNum(sellPrice)) {
+                  return res.status(400).send({ status: false, message: "Please provide valid sellPrice" })
+            }
+      }
+      if (stockPrice) {
+            if (!stockPrice) {
+                  return res.status(400).send({ status: false, message: "Please provide valid stockPrice" })
+            }
+            if (!isValidNum(stockPrice)) {
+                  return res.status(400).send({ status: false, message: "Please provide valid stockPrice" })
+            }
+      }
       let storeData = await Items.create(data)
       return res.status(201).send({ status: true, data: storeData })
 }
+catch (err) {
+      return res.status(500).send({ status: false, error: err.message })
+}
+}
 
 const fetchItem = async (req, res) => {
-      let fetchData = await Items.findAll({})
-      return res.status(200).send({ status: true, data: fetchData })
+      try {
+            let fetchData = await Items.findAll({})
+            return res.status(200).send({ status: true, data: fetchData })
+      }
+      catch (err) {
+            return res.status(500).send({ status: false, error: err.message })
+      }
 }
 
 const updateItem = async (req, res) => {
@@ -19,7 +55,7 @@ const updateItem = async (req, res) => {
             let itemId = req.params.itemId
             let data = req.body
 
-            if (!itemId) {
+            if (!itemId || !isValidNum(itemId)) {
                   return res.status(400).send({ status: false, message: "Please enter valid itemId!" })
             }
             const { productName, sellPrice, stockPrice } = data
@@ -45,11 +81,35 @@ const updateItem = async (req, res) => {
             }
 
             let updatedData = await Items.update(data, { where: { id: itemId } })
+            if (updatedData === null) {
+                  return res.status(400).send({ status: false, message: "Please enter valid item Id" })
+            }
             return res.status(200).send({ status: true, message: "update successfully", data: updatedData })
       }
       catch (err) {
             return res.status(500).send({ status: false, error: err.message })
       }
 }
+const deleteItem = async (req, res) => {
+      try {
+            let itemId = req.params.itemId
 
-module.exports = { createItem, fetchItem, updateItem }
+            if (!itemId || !isValidNum(itemId)) {
+                  return res.status(400).send({ status: false, message: "Please enter valid item Id" })
+            }
+            let deleteData = await Items.findOne({ where: { id: itemId } })
+            if (!deleteData) {
+                  return res.status(404).send({ status: false, message: "data not found to be deleted" })
+            }
+            if (deleteData.isDeleted.toString() === "true") {
+                  return res.status(400).send({ status: false, message: "item is already deleted" })
+            }
+            await Items.update({ isDeleted: true, deletedAt: Date.now() }, { where: { id: itemId } })
+            return res.status(200).send({ status: true, message: "deleted successfully" })
+      }
+      catch (err) {
+            return res.status(500).send({ status: false, error: err.message })
+      }
+}
+
+module.exports = { createItem, fetchItem, updateItem, deleteItem }
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,11 @@
 const router = require('express')()
-const { createItem, fetchItem, updateItem } = require('../controllers/itemController')
+const { createItem, fetchItem, updateItem, deleteItem } = require('../controllers/itemController')
 
 
 //items
 router.post('/createItems', createItem)
 router.get('/fetchItems', fetchItem)
 router.put('/updateItems/:itemId', updateItem)
+router.delete('/deleteItems/:itemId', deleteItem)
    
 module.exports = router 
